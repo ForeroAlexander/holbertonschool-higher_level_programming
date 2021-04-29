f1164ce77d84554e83560acade466953375d0a51
ForeroAlexander <foreroalex26@gmail.com>
2021-04-29 04:40:04 +0000

    fix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 2))
+    elif len(sys.argv) == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv)):
+        if i == 2:
+            continue
+        print("{:d}: {:s}".format(i, sys.argv[i]))
