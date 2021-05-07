ad77b2c6cb840c3079a1009e267af46d90135d4c
ForeroAlexander <foreroalex26@gmail.com>
2021-05-07 04:18:23 +0000

    search and replace:tada:

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = my_list[:]
+    j = 0
+    for item in new:
+        if item == search:
+            new[j] = replace
+        j += 1
+    return new
+    
