656f4f5de49fe0eef05390878a88241fa95a93ae
ForeroAlexander <foreroalex26@gmail.com>
2021-05-18 22:45:09 -0500

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class defintion"""
+
+
+class Square:
+    """Represents a square"""
+
+def __init__(self, size=0):
+    """Initialize a square
+    Args:
+        size (int): size of the square
+    Returns: none"""
+
+    self.__size = size
