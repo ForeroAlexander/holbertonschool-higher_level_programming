742cb92cd374c9ef502e76b1af5ff004e62530e1
ForeroAlexander <foreroalex26@gmail.com>
2021-05-18 20:20:39 -0500

    :tada: prints the first x elements of a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def safe_print_integer(my_list=[],x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-            except TypeError:
-                pass
-            except ValueError:
-                pass
-        print()
-        return count
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+    print()
+    return count
