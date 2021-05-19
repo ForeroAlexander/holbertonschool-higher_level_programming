7b2a96c36365f8ee8c3dc2750b0253278096aebd
ForeroAlexander <foreroalex26@gmail.com>
2021-05-18 20:39:30 -0500

    :tada: divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     list = []
-    for i in range(len(my_list)):
+    for i in range(list_length):
         try:
             res = (my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
-            res = 0x
-            print("division by 0x")
+            res = 0
+            print("division by 0")
         except TypeError:
             print("wrong type")
             res = 0
