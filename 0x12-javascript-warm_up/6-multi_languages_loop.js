4e68038a105b4e9d45d4e2647cd018fe562635c7
ForeroAlexander <foreroalex26@gmail.com>
2021-08-02 23:37:30 -0500

    fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; messages[i]; i++) {
+  console.log(messages[i]);
 }
