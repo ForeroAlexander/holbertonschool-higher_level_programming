b2db5a69d3080729df0d8787b71da1e8987aab3e
ForeroAlexander <foreroalex26@gmail.com>
2021-08-20 21:54:51 -0500

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
