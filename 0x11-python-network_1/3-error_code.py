223c64dc4fafea2a2937325722271612ced2a5e2
ForeroAlexander <foreroalex26@gmail.com>
2021-08-20 21:53:48 -0500

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""displays the body of the response (decoded in utf-8).
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urlopen(url) as response:
+            cont = response.read()
+            utf8 = cont.decode('utf-8')
+            print(utf8)
+    except HTTPError as status:
+        print("Error code: {}".format(status.code)
