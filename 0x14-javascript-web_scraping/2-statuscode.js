f33a0a0cc8b1afcd7ca38bad4f396a2bd8756de7
ForeroAlexander <foreroalex26@gmail.com>
2021-09-15 03:28:36 -0500

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url).on('response', function (response) {
+  console.log('code: %s', response.statusCode);
+});
